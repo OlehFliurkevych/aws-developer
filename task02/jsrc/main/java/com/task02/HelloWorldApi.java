b995cc5ed2e4386b46b78f81b32d1cc880dc34a6
Oleh Fliurkevych <oleh0809@gmail.com>
2024-01-17 20:56:28 +0200

    Change Lambda to process APIGatewayProxyRequestEvent

--- task02/jsrc/main/java/com/task02/HelloWorldApi.java
+++ task02/jsrc/main/java/com/task02/HelloWorldApi.java
@@ -2,29 +2,41 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.LambdaUrlConfig;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @LambdaHandler(lambdaName = "hello_world_api",
-	roleName = "hello_world_api-role",
-	isPublishVersion = true
+  roleName = "hello_world_api-role",
+  isPublishVersion = true
 )
 @LambdaUrlConfig(
-	authType = AuthType.NONE,
-	invokeMode = InvokeMode.BUFFERED
+  authType = AuthType.NONE,
+  invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorldApi implements RequestHandler<Object, Map<String, Object>> {
+public class HelloWorldApi implements
+  RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
-	}
+  private static final int SC_OK = 200;
+  private static final int SC_BAD_REQUEST = 400;
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  @Override
+  public APIGatewayProxyResponseEvent handleRequest(
+    APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+    context.getLogger().log(apiGatewayProxyRequestEvent.toString());
+    try {
+      return new APIGatewayProxyResponseEvent()
+        .withStatusCode(SC_OK)
+        .withBody(objectMapper.writeValueAsString(new Response("Hello from lambda")));
+    } catch (Exception exception) {
+      return new APIGatewayProxyResponseEvent()
+        .withStatusCode(SC_BAD_REQUEST)
+        .withBody(null);
+    }
+  }
 }
---
+++ task02/jsrc/main/java/com/task02/Response.java
@@ -0,0 +1,11 @@
+package com.task02;
+
+public class Response {
+
+  private final String message;
+
+  public Response(String message) {
+    this.message = message;
+  }
+
+}
